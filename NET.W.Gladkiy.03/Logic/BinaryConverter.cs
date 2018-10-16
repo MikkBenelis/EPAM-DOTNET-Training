7c2e4e01998cba5db3499189085b64ab4db735a3
MikkBenelis <codemikk@gmail.com>
2018-10-16 18:46:20 +0300

    Implemented BinaryConverter

--- NET.W.Gladkiy.03/Logic/BinaryConverter.cs
+++ NET.W.Gladkiy.03/Logic/BinaryConverter.cs
@@ -1,23 +1,49 @@
-﻿// Реализовать метод расширения для получения строкового двоичного 
-// представления вещественного числа двойной точности в формате 
-// IEEE 754 (желательно не использовать готовые классы-конверторы).
-
-namespace Logic.Converters
+﻿namespace Logic.Converters
 {
+    using System;
+    using System.Text;
+
     /// <summary>
     /// Binary converter
     /// </summary>
     public static class BinaryConverter
     {
+        #region PublicAPI
+
         /// <summary>Converts double to bit string</summary>
         /// <param name="number">double value to convert</param>
         /// <returns>string result of double bit representation</returns>
         public static string DoubleToBinaryString(double number)
         {
-            /// TODO
-            string result = string.Empty;
+            StringBuilder result = new StringBuilder();
+            long intBits = BitConverter.DoubleToInt64Bits(number);
+            result.Append(LongToBitString(intBits));
 
-            return result;
+            return result.ToString();
         }
+
+        #endregion PublicAPI
+
+        #region PrivateAPI
+
+        /*
+        private static unsafe long DoubleToInt64Bits(double value)
+        {
+            return *((long*)&value);
+        }
+        */
+        
+        private static string LongToBitString(long number)
+        {
+            var result = new StringBuilder();
+            for (int i = (sizeof(long) * 8) - 1; i >= 0; i--)
+            {
+                result.Append((number >> i) & 1);
+            }
+
+            return result.ToString().PadLeft(sizeof(long) * 8, '0');
+        }
+
+        #endregion PrivateAPI
     }
 }
