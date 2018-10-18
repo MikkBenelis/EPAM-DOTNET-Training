8b6de6ea342638781b3bc05effd7a2aee1e70237
MikkBenelis <codemikk@gmail.com>
2018-10-18 17:58:47 +0300

    Created separate class for Stein's algorithm

--- NET.W.Gladkiy.03/Logic/GCDOperations.cs
+++ NET.W.Gladkiy.03/Logic/GCDOperations.cs
@@ -6,7 +6,7 @@
     /// <summary>
     /// Finding GCD algorithms
     /// </summary>
-    public static class GCDOperations
+    public partial class GCDOperations
     {
         #region PublicAPI
 
@@ -51,30 +51,6 @@
             for (int i = 1; i < nums.Length; i++)
             {
                 result = EuclideanI(result, nums[i]);
-            }
-
-            watch.Stop();
-            execTime = watch.ElapsedMilliseconds;
-            return result;
-        }
-
-        /// <summary>Stein's GCD algorithm</summary>
-        /// <param name="execTime">long reference to execution time result</param>
-        /// <param name="nums">int array to find GCD</param>
-        /// <returns>Greatest common divisor of given numbers</returns>
-        public static int Stein(out long execTime, params int[] nums)
-        {
-            if (nums == null || nums.Length < 2)
-            {
-                throw new ArgumentException();
-            }
-
-            var watch = Stopwatch.StartNew();
-            
-            int result = nums[0];
-            for (int i = 1; i < nums.Length; i++)
-            {
-                result = SteinLogic(result, nums[i]);
             }
 
             watch.Stop();
@@ -135,57 +111,5 @@
         }
 
         #endregion PrivateEuclideanAPI
-
-        #region PrivateSteinAPI
-
-        private static int SteinLogic(int a, int b)
-        {
-            if (a == 0)
-            {
-                return b;
-            }
-
-            if (b == 0)
-            {
-                return a;
-            }
-
-            if (a == b)
-            {
-                return a;
-            }
-
-            if ((a & 1) == 0 && (b & 1) == 0)
-            {
-                return SteinLogic(a >> 1, b >> 1) << 1;
-            }
-            else
-            {
-                if ((a & 1) == 0 && (b & 1) != 0)
-                {
-                    return SteinLogic(a >> 1, b);
-                }
-                else
-                {
-                    if ((b & 1) == 0)
-                    {
-                        return SteinLogic(a, b >> 1);
-                    }
-                    else
-                    {
-                        if (a > b)
-                        {
-                            return SteinLogic((a - b) >> 1, b);
-                        }
-                        else
-                        {
-                            return SteinLogic(a, (b - a) >> 1);
-                        }
-                    }
-                }
-            }
-        }
-
-        #endregion PrivateSteinAPI
     }
 }
