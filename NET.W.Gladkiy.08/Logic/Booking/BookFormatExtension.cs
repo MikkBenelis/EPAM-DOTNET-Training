3a892fe074fcc14cb4658bb0b23105d368f963cf
MikkBenelis <codemikk@gmail.com>
2018-11-04 12:55:52 +0300

    Added book string format extenstion

---
+++ NET.W.Gladkiy.08/Logic/Booking/BookFormatExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace BookFormatExtension
+{
+    using System.Text;
+    using Logic.BookingSystem;
+
+    public static class BookFormatExtension
+    {
+        /// <summary>Gets book string representation with price</summary>
+        /// <param name="book">book to get string representation</param>
+        /// <returns>short book string representation with price</returns>
+        public static string ToRegionalPricedFormat(this Book book)
+        {
+            var sb = new StringBuilder();
+            sb.Append(book.ToShortString() + ", ");
+            sb.AppendFormat("{0:C}", book.Price);
+            return sb.ToString();
+        }
+    }
+}
