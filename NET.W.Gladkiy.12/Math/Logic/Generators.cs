e8f7ee83697c78f2a63a90bee443fee45fd563a2
MikkBenelis <codemikk@gmail.com>
2018-11-13 20:01:34 +0300

    Implemented binary search and fibonacci generator

---
+++ NET.W.Gladkiy.12/Math/Logic/Generators.cs
@@ -0,0 +1,26 @@
+﻿namespace Math
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Generators
+    {
+        public static IEnumerable<int> Fibonacci(int x)
+        {
+            if (x <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            int prev = 0;
+            int next = 1;
+            for (int i = 0; i < x; i++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+                yield return sum;
+            }
+        }
+    }
+}
---
+++ NET.W.Gladkiy.12/Math/Logic/Searching.cs
@@ -0,0 +1,57 @@
+﻿namespace Math
+{
+    using System;
+
+    public static class Searching
+    {
+        public static bool BinarySearch<T>(T[] array, T smthToFind) where T : IComparable<T>
+        {
+            if (array == null || smthToFind == null || !CheckIfSorted(array))
+            {
+                throw new ArgumentException();
+            }
+            
+            int min = 0;
+            int max = array.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (smthToFind.CompareTo(array[mid]) == 0)
+                {
+                    return true;
+                }
+                else if (smthToFind.CompareTo(array[mid]) < 0)
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool CheckIfSorted<T>(T[] array) where T : IComparable<T>
+        {
+            if (array.Length == 1)
+            {
+                return true;
+            }
+
+            T last = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (last.CompareTo(array[i]) > 0)
+                {
+                    return false;
+                }
+
+                last = array[i];
+            }
+
+            return true;
+        }
+    }
+}
