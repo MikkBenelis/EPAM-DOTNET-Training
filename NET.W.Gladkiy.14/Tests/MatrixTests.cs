f383c4c5d117c79129e55358a79ca6e0523fe8b3
MikkBenelis <codemikk@gmail.com>
2018-11-18 14:30:49 +0300

    Implemented queue and matrices tests

---
+++ NET.W.Gladkiy.14/Tests/MatrixTests.cs
@@ -0,0 +1,133 @@
+﻿namespace NUnitTests
+{
+    using System;
+    using Logic.Generic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MatrixTests
+    {
+        [Test]
+        public void CreateSquareMatrixTest()
+        {
+            var sqM = new SquareMatrix<int>(3);
+            Assert.That(
+                () => sqM.SetElements(new int[,]
+                {
+                    { 1, 2, 3 },
+                    { 1, 2, 3 },
+                    { 1, 2, 3 }
+                }),
+                Throws.Nothing);
+
+            Assert.That(
+                () => sqM.SetElements(new int[,]
+                {
+                    { 1, 2, 3, 4, 5 },
+                    { 1, 2, 3, 4, 5 },
+                    { 1, 2, 3, 4, 5 }
+                }),
+                Throws.Exception.TypeOf<ArgumentException>());
+
+            Assert.That(
+                () => sqM.SetElements(null), 
+                Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void CreateSymmetricMatrixTest()
+        {
+            var symM = new SymmetricMatrix<int>(3);
+
+            Assert.That(
+                () => symM.SetElements(new int[,]
+                {
+                    { 1, 2, 3 },
+                    { 2, 3, 1 },
+                    { 3, 1, 2 }
+                }),
+                Throws.Nothing);
+            
+            Assert.That(
+                () => symM.SetElements(new int[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 2, 2 },
+                    { 3, 3, 3 }
+                }),
+                Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void CreateDiagonalMatrixTest()
+        {
+            var diagM = new DiagonalMatrix<int>(3);
+
+            Assert.That(
+                () => diagM.SetElements(new int[,]
+                {
+                    { 1, 0, 0 },
+                    { 0, 2, 0 },
+                    { 0, 0, 3 }
+                }),
+                Throws.Nothing);
+
+            Assert.That(
+                () => diagM.SetElements(new int[,]
+                {
+                    { 1, 0, 1 },
+                    { 0, 2, 0 },
+                    { 1, 0, 3 }
+                }),
+                Throws.Exception.TypeOf<ArgumentException>());
+
+            Assert.That(
+                () => diagM.SetElements(new int[] { 1, 2, 3 }),
+                Throws.Nothing);
+
+            Assert.That(
+                () => diagM.SetElements(new int[] { 1, 2, 3, 4, 5 }),
+                Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void MatrixSumTest()
+        {
+            var symM = new SymmetricMatrix<int>(3);
+            symM.SetElements(new int[,]
+            {
+                { 1, 2, 3 },
+                { 2, 3, 1 },
+                { 3, 1, 2 }
+            });
+                
+            var diagM = new DiagonalMatrix<int>(3);
+            diagM.SetElements(new int[,]
+            {
+                { 1, 0, 0 },
+                { 0, 2, 0 },
+                { 0, 0, 3 }
+            });
+
+            var correctSum = new SquareMatrix<int>(3);
+            correctSum.SetElements(new int[,]
+            {
+                { 2, 2, 3 },
+                { 2, 5, 1 },
+                { 3, 1, 5 }
+            });
+
+            var incorrectSum = new SquareMatrix<int>(3);
+            incorrectSum.SetElements(new int[,]
+            {
+                { 0, 0, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 0 }
+            });
+
+            var result = symM.AddMatrix(diagM, delegate(int a, int b) { return a + b; });
+            Assert.That(result, Is.EqualTo(correctSum));
+            Assert.That(result, Is.Not.EqualTo(incorrectSum));
+        }
+    }
+}
---
+++ NET.W.Gladkiy.14/Tests/QueueTests.cs
@@ -0,0 +1,78 @@
+﻿namespace NUnitTests
+{
+    using Logic.Generic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class QueueTests
+    {
+        [Test]
+        public void CreateClearTest()
+        {
+            var q1 = new Queue<int>();
+            Assert.That(q1.Capacity, Is.EqualTo(Queue<int>.DEFAULT_CAPAITY));
+            Assert.That(q1.GrowFactor, Is.EqualTo(Queue<int>.DEFAULT_GROW_FACTOR));
+
+            var q2 = new Queue<int>(10);
+            Assert.That(q2.Capacity, Is.EqualTo(10));
+            Assert.That(q2.GrowFactor, Is.EqualTo(Queue<int>.DEFAULT_GROW_FACTOR));
+
+            var q3 = new Queue<int>(10, 1.5);
+            Assert.That(q3.Capacity, Is.EqualTo(10));
+            Assert.That(q3.GrowFactor, Is.EqualTo(1.5));
+
+            q1.Clear();
+            Assert.That(q1.Count, Is.EqualTo(0));
+            Assert.That(q1.Capacity, Is.EqualTo(Queue<int>.DEFAULT_CAPAITY));
+        }
+
+        [Test]
+        public void EnqueueTrimTest()
+        {
+            var q = new Queue<int>();
+
+            q.Enqueue(10);
+            Assert.That(q.Capacity, Is.EqualTo(1));
+
+            q.Enqueue(15);
+            Assert.That(q.Capacity, Is.EqualTo(2));
+
+            q.Enqueue(30);
+            Assert.That(q.Capacity, Is.EqualTo(4));
+
+            q.TrimToSize();
+            Assert.That(q.Capacity, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void PeekDequeueTest()
+        {
+            var q = new Queue<int>();
+
+            q.Enqueue(10);
+            q.Enqueue(15);
+            q.Enqueue(30);
+
+            Assert.That(q.Peek(), Is.EqualTo(10));
+            Assert.That(q.Dequeue(), Is.EqualTo(10));
+            Assert.That(q.Dequeue(), Is.EqualTo(15));
+            Assert.That(q.Dequeue(), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void ContainsToArrayTest()
+        {
+            var q = new Queue<int>();
+
+            q.Enqueue(10);
+            q.Enqueue(15);
+            q.Enqueue(30);
+
+            Assert.That(q.Contains(10), Is.True);
+            Assert.That(q.Contains(100), Is.False);
+
+            var intArray = new int[] { 10, 15, 30 };
+            Assert.That(q.ToArray(), Is.EqualTo(intArray));
+        }
+    }
+}
